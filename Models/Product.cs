abeaf0e0ae03d97ec40b91119ac242d8f61bc960
mohamedkamalic <mohammedkamal126@gmail.com>
2020-05-28 22:04:41 +0200

    addded product module

---
+++ Models/Product.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        public String Name { get; set; }
+
+
+        [Range(1, double.MaxValue, ErrorMessage = "You must Specify The Price")]
+        public int Price { get; set; }
+
+        // [Required]
+        public String Image { get; set; }
+        public String description { get; set; }
+    }
+}
