fddf5383d63d70ee24e59776fd91e167872b344f
mohamed tarek <mohamedtarek23112@gamil.com>
2020-05-29 11:19:34 +0200

    category module added

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Online_Store.Models
+{
+    [Table("Category")]
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+
+        public int Number_of_products { get; set; }
+
+    }
+}
