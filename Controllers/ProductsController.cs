e428c099ea73cb56109789515c860463106560ca
mohamedkamalic <mohammedkamal126@gmail.com>
2020-05-30 14:13:31 +0200

    index action

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using OnlineStore.Models;
+using System.Web.UI.WebControls;
+
+namespace OnlineStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProductsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Products
+        public ActionResult Index()
+        {
+
+            var products = _context.Products.ToList();
+
+            return View("CatalogPage", products);
+        }
+    }
+}
