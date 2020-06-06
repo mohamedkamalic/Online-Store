526210427df222a109ec88ec38604e74166b6fec
Abdokhaled-99 <43261845+Abdokhaled-99@users.noreply.github.com>
2020-06-07 00:53:23 +0200

    Cart model

---
+++ Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using OnlineStore.Models;
+
+namespace OnlineStore.Models
+{
+    [Table("Cart")]
+    public class Cart
+    {
+        [Key]
+        [ForeignKey("Product")]
+        public int Product_id { get; set; }
+        
+        public virtual Product Product { get; set; }
+        public DateTime Added_at { get; set; }
+    }
+}
