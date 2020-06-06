c35839028dddd64fc49826da1c23f3b8d11c105e
Abdokhaled-99 <43261845+Abdokhaled-99@users.noreply.github.com>
2020-06-07 01:19:38 +0200

    add Cart To database

---
+++ Migrations/202006062317449_addCarttoDatabase.cs
@@ -0,0 +1,30 @@
+namespace OnlineStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addCarttoDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cart",
+                c => new
+                    {
+                        Product_id = c.Int(nullable: false),
+                        Added_at = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Product_id)
+                .ForeignKey("dbo.Product", t => t.Product_id)
+                .Index(t => t.Product_id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Cart", "Product_id", "dbo.Product");
+            DropIndex("dbo.Cart", new[] { "Product_id" });
+            DropTable("dbo.Cart");
+        }
+    }
+}
