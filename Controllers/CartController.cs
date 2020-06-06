c64e3855fb7af55a10cd69fbc93cfe47df97398a
mohamedkamalic <mohammedkamal126@gmail.com>
2020-06-07 01:27:08 +0200

    add index action

---
+++ Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineStore.Models;
+
+namespace OnlineStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CartController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Cart
+        public ActionResult Index()
+        {
+
+            var results = (from item in _context.Products.ToList() where _context.Cart.Select(c => c.Product_id).Contains(item.Id) select item).ToList();
+            return PartialView("_View", results);
+
+        }
+    }
+}
